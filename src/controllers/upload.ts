3ffa3c69d7de1bcc372ce1ae6c6bdbdae726743e
FroseWong <tibafrose@gmail.com>
2024-05-21 22:01:50 +0800

    暫時註解upload

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -1,51 +1,51 @@
-import { RequestHandler } from 'express';
-import { errorResponse, handleErrorAsync } from '@/utils/errorHandler';
-import { successResponse } from '@/utils/successHandler';
-import firebaseAdmin from '@/service/firebase'
-import { v4 as uuidv4 } from 'uuid'
+// import { RequestHandler } from 'express';
+// import { errorResponse, handleErrorAsync } from '@/utils/errorHandler';
+// import { successResponse } from '@/utils/successHandler';
+// import firebaseAdmin from '@/service/firebase'
+// import { v4 as uuidv4 } from 'uuid'
 
-const bucket = firebaseAdmin.storage().bucket()
-export const uploadFile: RequestHandler = handleErrorAsync(async (req, res, _next) => {
-  // 取得上傳的檔案資訊
-  const file = req.file
-  if (!file) {
-    return errorResponse(400, '未找到檔案')
-  }
+// const bucket = firebaseAdmin.storage().bucket()
+// export const uploadFile: RequestHandler = handleErrorAsync(async (req, res, _next) => {
+//   // 取得上傳的檔案資訊
+//   const file = req.file
+//   if (!file) {
+//     return errorResponse(400, '未找到檔案')
+//   }
 
-  // 基於檔案的原始名稱建立一個 blob 物件
-  const blob = bucket.file(`images/${uuidv4()}.${file.originalname.split('.').pop()}`);
-  // 建立一個可以寫入 blob 的物件
-  const blobStream = blob.createWriteStream()
+//   // 基於檔案的原始名稱建立一個 blob 物件
+//   const blob = bucket.file(`images/${uuidv4()}.${file.originalname.split('.').pop()}`);
+//   // 建立一個可以寫入 blob 的物件
+//   const blobStream = blob.createWriteStream()
 
-  // 監聽上傳狀態,當上傳完成時,會觸發 finish 事件
-  blobStream.on('finish', () => {
-    // 設定檔案的存取權限
-    const config = {
-      action: 'read' as const,
-      expires: '2500-12-31' // 使用 'YYYY-MM-DD' 格式
-    }
+//   // 監聽上傳狀態,當上傳完成時,會觸發 finish 事件
+//   blobStream.on('finish', () => {
+//     // 設定檔案的存取權限
+//     const config = {
+//       action: 'read' as const,
+//       expires: '2500-12-31' // 使用 'YYYY-MM-DD' 格式
+//     }
 
-    // 取得檔案的網址
-    blob.getSignedUrl(config, (err, imgUrl) => {
-      if (err || !imgUrl) {
-        return errorResponse(404, '上傳失敗')
-      }
-      res.status(200).json(
-        successResponse({
-          message: '上傳成功',
-          data: { imgUrl },
-        }),
-      );
-    })
-  })
+//     // 取得檔案的網址
+//     blob.getSignedUrl(config, (err, imgUrl) => {
+//       if (err || !imgUrl) {
+//         return errorResponse(404, '上傳失敗')
+//       }
+//       res.status(200).json(
+//         successResponse({
+//           message: '上傳成功',
+//           data: { imgUrl },
+//         }),
+//       );
+//     })
+//   })
 
-  // 如果上傳過程中發生錯誤,會觸發 error 事件
-  blobStream.on('error', () => {
-    return errorResponse(404, '傳過程中發生錯誤')
-  })
+//   // 如果上傳過程中發生錯誤,會觸發 error 事件
+//   blobStream.on('error', () => {
+//     return errorResponse(404, '傳過程中發生錯誤')
+//   })
 
-  // 將檔案的 buffer 寫入 blobStream
-  blobStream.end(file.buffer)
+//   // 將檔案的 buffer 寫入 blobStream
+//   blobStream.end(file.buffer)
 
-});
+// });
 
