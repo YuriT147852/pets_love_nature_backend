296c6a1f6990198f3b81247341528610e76c6c57
BigTO <d22495521@gmail.com>
2024-06-15 16:56:23 +0800

    feat:新增chat資料庫及對話

--- src/utils/socket-io.ts
+++ src/utils/socket-io.ts
@@ -1,8 +1,10 @@
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import { Server as HTTPServer } from 'http';
-import { SocketUser } from '@/types/socket';
+import { SocketUser, SocketMsg } from '@/types/socket';
 import CustomerModel from '@/models/customer';
+import ChatModel from '@/models/chat';
 import jsonWebToken, { type JwtPayload } from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 let io: SocketIOServer;
 let token: string;
@@ -42,6 +44,7 @@ export function connectSocketIO(server: HTTPServer) {
                     _id: decoded.userId
                 });
 
+                //如果沒有該使用者報錯
                 if (!result) {
                     emitErrorMsg(socket, '用戶驗證失敗');
                     return next(new Error('User not found'));
@@ -63,19 +66,62 @@ export function connectSocketIO(server: HTTPServer) {
     });
 
     io.on('connection', (socket: Socket) => {
-        //加入個別房間
+        //client加入個別房間
         socket.on('join room', async data => {
-            const { userId, role } = data as SocketUser;
+            try {
+                const { userId, role } = data as SocketUser;
 
-            if (!userId || role !== 'client') {
-                return;
+                if (!userId || role !== 'client') {
+                    emitErrorMsg(socket, 'userId與role錯誤');
+                    return;
+                }
+
+                const roomResult = await ChatModel.findOne({ userId });
+
+                //如果不到就創一個新的
+                if (!roomResult) {
+                    await ChatModel.create({ userId, messageList: [] });
+                }
+                await socket.join(userId);
+            } catch (error) {
+                emitErrorMsg(socket, '加入房間失敗');
             }
-            await socket.join(userId);
         });
 
-        socket.on('chat message', (msg: string) => {
-            console.log(msg);
-            io.emit('chat message', `回傳${msg}`);
+        socket.on('message', async data => {
+            const { message, userId, role } = data as SocketMsg;
+
+            if (!userId) {
+                emitErrorMsg(socket, 'userId不存在');
+                return;
+            }
+
+            if (role === 'client') {
+                //製作message
+                const newMessage = {
+                    role: 'client',
+                    read: false,
+                    message,
+                    chatId: new mongoose.Types.ObjectId('661a9a9fa892ea2a833a1009')
+                };
+                console.log(newMessage);
+                await ChatModel.findOneAndUpdate({ userId }, { $push: { messageList: newMessage } });
+                //只傳給admin
+                io.emit('admin message', newMessage);
+            }
+
+            if (role === 'admin') {
+                //製作message
+                const newMessage = {
+                    role: 'admin',
+                    read: false,
+                    message,
+                    chatId: new mongoose.Types.ObjectId('661a9a9fa892ea2a833a1009')
+                };
+                await ChatModel.findOneAndUpdate({ userId }, { $push: { messageList: newMessage } });
+                //只傳給特定人
+                io.to(userId).emit('message', newMessage);
+            }
         });
 
         socket.on('disconnect', () => {
