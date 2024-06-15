f7774ddb3ecd6af0b3384669b1ffd91e6bb14df0
YuriT147852 <twy0171@gmail.com>
2024-06-15 16:39:57 +0800

    fix: 移除未使用類別

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,25 +1,19 @@
 import type { RequestHandler } from 'express';
 import { errorResponse, handleErrorAsync } from '@/utils/errorHandler';
 import { successResponse } from '@/utils/successHandler';
-import ProductModel, { IProduct } from '@/models/product';
-import ProductSpecModel, { IProductSpec } from '@/models/productSpec';
-import { ICreateProduct, IShowProduct } from '@/types/product';
-import { PipelineStage } from 'mongoose';
+import CommentModel from '@/models/comment';
 
 
 // 取得所有商品的評論
 export const getAllCommentList: RequestHandler = handleErrorAsync(async (_req, res, next) => {
-  const result = await ProductSpecModel.find({}).populate({
-    path: 'productId',
-    select: 'title subtitle description star category otherInfo imageGallery'
-  });
+  const result = await CommentModel.find({});
   if (result.length === 0) {
-    next(errorResponse(404, '無商品資料'));
+    next(errorResponse(404, '無評論資料'));
     return;
   }
   res.status(200).json(
     successResponse({
-      message: '取得商品資料成功',
+      message: '取得所有商品的評論成功',
       data: result,
     }),
   );
@@ -27,17 +21,14 @@ export const getAllCommentList: RequestHandler = handleErrorAsync(async (_req, r
 
 // 取得單一商品資訊的評論
 export const getFilterCommentList: RequestHandler = handleErrorAsync(async (_req, res, next) => {
-  const result = await ProductSpecModel.find({}).populate({
-    path: 'productId',
-    select: 'title subtitle description star category otherInfo imageGallery'
-  });
+  const result = await CommentModel.find({});
   if (result.length === 0) {
-    next(errorResponse(404, '無商品資料'));
+    next(errorResponse(404, '無評論資料'));
     return;
   }
   res.status(200).json(
     successResponse({
-      message: '取得商品資料成功',
+      message: '取得所有商品的評論成功',
       data: result,
     }),
   );
@@ -63,17 +54,14 @@ export const createComment: RequestHandler = handleErrorAsync(async (_req, res, 
 
 // 取得單一商品資訊的評論
 export const getNoCommentOrderIdList: RequestHandler = handleErrorAsync(async (_req, res, next) => {
-  const result = await ProductSpecModel.find({}).populate({
-    path: 'productId',
-    select: 'title subtitle description star category otherInfo imageGallery'
-  });
+  const result = await CommentModel.find({});
   if (result.length === 0) {
-    next(errorResponse(404, '無商品資料'));
+    next(errorResponse(404, '無評論資料'));
     return;
   }
   res.status(200).json(
     successResponse({
-      message: '取得商品資料成功',
+      message: '取得所有商品的評論成功',
       data: result,
     }),
   );
@@ -81,17 +69,14 @@ export const getNoCommentOrderIdList: RequestHandler = handleErrorAsync(async (_
 
 // 取得消費者未評論的該筆訂單及商品資訊
 export const getComment: RequestHandler = handleErrorAsync(async (_req, res, next) => {
-  const result = await ProductSpecModel.find({}).populate({
-    path: 'productId',
-    select: 'title subtitle description star category otherInfo imageGallery'
-  });
+  const result = await CommentModel.find({});
   if (result.length === 0) {
-    next(errorResponse(404, '無商品資料'));
+    next(errorResponse(404, '無評論資料'));
     return;
   }
   res.status(200).json(
     successResponse({
-      message: '取得商品資料成功',
+      message: '取得所有商品的評論成功',
       data: result,
     }),
   );
