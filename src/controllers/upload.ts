55ec18e81c8c63cabe7eb4303626f74d336117f5
YuriT147852 <twy0171@gmail.com>
2024-06-22 01:35:16 +0800

    fix: upload

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -7,7 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 const bucket = firebaseAdmin.storage().bucket()
 export const uploadFile = (req: Request, res: Response, next: NextFunction) => {
   // 取得上傳的檔案資訊
-  const file = req.file
+  const file = req.file as Express.Multer.File
   if (!file) {
     return next(errorResponse(400, '未找到檔案'));
   }
