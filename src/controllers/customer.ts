0b1c931ed5432c0d3353c5721d3afa2961d208ba
BigTO <d22495521@gmail.com>
2024-06-22 17:47:30 +0800

    feat:新增後台消費者api

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -232,3 +232,71 @@ export const updateAccountStatus: RequestHandler = handleErrorAsync(async (req, 
         })
     );
 });
+
+export const getCustomerList: RequestHandler = handleErrorAsync(async (req, res, _next) => {
+    const { page = 1, limit, sortOrder, sortBy, requestSame, search } = req.query;
+
+    // 默認 1 頁顯示 10 筆
+    const pageSize = limit ? parseInt(limit as string, 10) : 10;
+    // 獲取總筆數
+    const totalDocuments = await CustomerModel.countDocuments();
+    // 獲取總頁數
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+    //跳過比數
+    const skip = (Number(page) - 1) * pageSize;
+    //大小排序
+    const filter = sortBy === '1' ? 1 : -1;
+
+    if (search) {
+        const text: string = search as string;
+        //模糊搜
+        const regex = new RegExp(text);
+        const filterRegex = { $regex: regex };
+        const filterHandler = requestSame === '1' ? search : filterRegex;
+        const result = await CustomerModel.find({ email: filterHandler });
+
+        const resData = {
+            data: result,
+            page: {
+                totalPages: 1,
+                nowPage: 1
+            }
+        };
+        res.status(200).json(
+            successResponse({
+                message: '查看帳號列表成功',
+                data: resData
+            })
+        );
+    }
+
+    const sortOptions: Record<string, 1 | -1> = {};
+
+    if (sortOrder === 'accountStatus') {
+        sortOptions['accountStatus'] = filter;
+        sortOptions['email'] = 1;
+    } else if (sortOrder === 'email') {
+        sortOptions['email'] = filter;
+        sortOptions['accountStatus'] = 1;
+    } else {
+        sortOptions['email'] = 1;
+        sortOptions['accountStatus'] = 1;
+    }
+
+    const result = await CustomerModel.find().skip(skip).limit(pageSize).sort(sortOptions);
+
+    const resData = {
+        data: result,
+        page: {
+            totalPages,
+            nowPage: page
+        }
+    };
+
+    res.status(200).json(
+        successResponse({
+            message: '查看帳號列表成功',
+            data: resData
+        })
+    );
+});
